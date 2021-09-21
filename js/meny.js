cb4fe33c1b66527f873db2683592ec7600f09905
FancyNorway <s302061@oslomet.no>
2021-09-21 18:29:25 +0200

    endringer

---
+++ js/meny.js
@@ -0,0 +1,28 @@
+document.getElementById("label_foretter").addEventListener("click", visForetter);
+document.getElementById("label_hovedretter").addEventListener("click", visHovedretter);
+document.getElementById("label_dessert").addEventListener("click", visDessert);
+document.getElementById("visAlleMenyene").addEventListener("keydown", visAlleMenyene);  
+
+function visForetter() {
+    document.getElementById("hovedretter").style.display = "none";
+    document.getElementById("dessert").style.display = "none";
+    document.getElementById("foretter").style.display = "table";
+}
+
+function visHovedretter() {
+    document.getElementById("dessert").style.display = "none";
+    document.getElementById("foretter").style.display = "none";
+    document.getElementById("hovedretter").style.display = "table";
+}
+
+  function visDessert() {
+    document.getElementById("hovedretter").style.display = "none";
+    document.getElementById("foretter").style.display = "none";
+    document.getElementById("dessert").style.display = "table";
+}
+
+function visAlleMenyene() {
+    document.getElementById("foretter").style.display = "table";
+    document.getElementById("hovedretter").style.display = "table";
+    document.getElementById("dessert").style.display = "table";
+}
